4b9d88e9ae2e12ebb580442e5bdf2f9109d77bec
mapfeiff <mapfeiff@uci.edu>
2023-11-17 12:34:15 -0800

    changed example reactant to use stereo_remove_and_canonicalizer

--- rexgen_direct/rank_diff_wln/directcandranker.py
+++ rexgen_direct/rank_diff_wln/directcandranker.py
@@ -174,8 +174,7 @@ if __name__ == '__main__':
         print("----------")
         react = '[CH3:26][c:27]1[cH:28][cH:29][cH:30][cH:31][cH:32]1.[Cl:18][C:19](=[O:20])[O:21][C:22]([Cl:23])([Cl:24])[Cl:25].[NH2:1][c:2]1[cH:3][cH:4][c:5]([Br:17])[c:6]2[c:10]1[O:9][C:8]([CH3:11])([C:12](=[O:13])[O:14][CH2:15][CH3:16])[CH2:7]2'
         react = "CCOC(=O)C1=CC=C[C@@H](NC(=O)OC(C)(C)C)C1"
-        react = Chem.RemoveStereochemistry(react)
-        react = Chem.CanonSmiles(react)
+        react = stereo_remove_and_canonicalize(react)
         print(react)
         print("----------")
         #get the score for the example reactant
